aba869695f042be19a88286173c0dddf332f175f
Nataliia <matveeva.github@mail.ru>
2026-08-23 10:44:26 +0300

    delete whole client service

--- services/client/apps/__init__.py
+++
@@ -1,2 +0,0 @@
-# Будущие Django-приложения client: accounts, subscriptions, notifications, consumer.
-# Здесь появится логика регистрации пользователей, подписок и отправки уведомлений.
--- services/client/config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Пакет настроек client.
-
-Celery импортируем здесь, чтобы @shared_task в apps/*/tasks.py привязывались к
-нашему приложению, а не к дефолтному.
-"""
-from config.celery_app import celery_app
-
-__all__ = ["celery_app"]
